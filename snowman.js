618c1d5ff1e191600744ca254c1169a54ce14192
Your Name <ckoutianov@mcad.edu>
2026-01-24 14:52:16 -0800

    Update snowman.js style

--- snowman.js
+++ snowman.js
@@ -1,13 +1,15 @@
 let grid = undefined;
 function setup() {
     createCanvas(1000, 800);
-    background("#d8c3a5");
+    background("#587291");
     grid = loadImage("../img/100px_grid.png");
 }
 function draw() {
     background(grid);
     //legs
-    fill("#f1f1f1");
-    stroke(100, 250, 100);
+    fill("#1ccad8");
+    stroke("#2f97c1");
+    //left leg (x from the left, y from the bottom, width, optional height)
+    ellipse(350, 600, 200);
 
 }
